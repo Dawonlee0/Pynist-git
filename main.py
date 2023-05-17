99fc1faaa958dfcbb0e7e04719222008959a405b
Dawonlee0 <ttoyr219@naver.com>
2023-05-17 14:23:24 +0900

    v5

--- main.py
+++ main.py
@@ -1,48 +1,101 @@
 from tkinter import *
 
-# 게임 설명 창 열기
-def open_desc_window():
-    global desc_window
-    
-    if desc_window:
-        desc_window.lift()
-        return
 
-    desc_window = Toplevel()
-    desc_window.title("게임 설명")
-
-    desc_label = Label(desc_window, text="게임 설명", font=("Arial", 24))
-    desc_label.pack(pady=20)
-
-    desc_text = Text(desc_window, width=50, height=10)
-    desc_text.pack(padx=20, pady=20)
-
-    desc_text.insert(END, "게임 설명을 입력하세요.")
-
-    desc_window.mainloop()
-
-# 윈도우 생성
-window = Tk()
-window.title("게임 제목")
+# 메인 창 생성
+main_window = Tk()
+main_window.title("게임")
 
 # 제목 라벨 생성
-title_label = Label(window, text="게임 제목", font=("Arial", 24))
-title_label.pack(pady=20)
+title_label = Label(main_window, text="게임", font=("Arial", 30))
+title_label.pack()
 
-# 게임 설명 창 생성
-desc_window = None
+def open_stage_window():
+    # 스테이지 선택 창 열기
+    main_window.withdraw()  # 메인 창 숨기기
+    stage_window = Tk()
+    stage_window.title("스테이지 선택")
 
-# 버튼 3개 생성
-start_button = Button(window, text="게임 시작")
-story_button = Button(window, text="스토리 설명")
-desc_button = Button(window, text="게임 설명", command=open_desc_window)
-quit_button = Button(window, text="끝내기", command=window.destroy)
+    # 스테이지 선택 라벨 생성
+    stage_label = Label(stage_window, text="스테이지 선택", font=("Arial", 30))
+    stage_label.pack()
+
+    # 스테이지 버튼 생성
+    button1 = Button(stage_window, text="1번")
+    button1.pack(side="left", padx=10, pady=10)
+    button2 = Button(stage_window, text="2번")
+    button2.pack(side="left", padx=10, pady=10)
+    button3 = Button(stage_window, text="3번")
+    button3.pack(side="left", padx=10, pady=10)
+    button4 = Button(stage_window, text="4번")
+    button4.pack(side="left", padx=10, pady=10)
+
+    def back_to_main():
+        # 이전으로 돌아가기
+        stage_window.destroy()  # 스테이지 선택 창 닫기
+        main_window.deiconify()  # 메인 창 다시 보이기
+
+    # 이전으로 돌아가는 버튼 생성
+    back_button = Button(stage_window, text="이전", command=back_to_main)
+    back_button.pack(pady=10)
+
+def open_story_window():
+    # 스토리 설명 창 열기
+    main_window.withdraw()  # 메인 창 숨기기
+    story_window = Tk()
+    story_window.title("스토리 설명")
+
+    # 스토리 설명 라벨 생성
+    story_label = Label(story_window, text="스토리 설명", font=("Arial", 30))
+    story_label.pack()
+
+    # 스토리 설명 텍스트 입력 공간 생성
+    story_text = Text(story_window, width=50, height=10)
+    story_text.pack(pady=10)
+
+    def back_to_main():
+        # 이전으로 돌아가기
+        story_window.destroy()  # 스토리 설명 창 닫기
+        main_window.deiconify()  # 메인 창 다시 보이기
+
+    # 이전으로 돌아가는 버튼 생성
+    back_button = Button(story_window, text="이전", command=back_to_main)
+    back_button.pack(pady=10)
+
+def open_desc_window():
+    # 게임 설명 창 열기
+    main_window.withdraw()  # 메인 창 숨기기
+    desc_window = Tk()
+    desc_window.title("게임 설명")
+
+    # 게임 설명 라벨 생성
+    desc_label = Label(desc_window, text="게임 설명", font=("Arial", 30))
+    desc_label.pack()
+
+    # 게임 설명 텍스트 입력 공간 생성
+    desc_text = Text(desc_window, width=50, height=10)
+    desc_text.pack(pady=10)
+    
+    # 이전으로 돌아가기
+    def back_to_main():
+        desc_window.destroy()  # 게임 설명 창 닫기
+        main_window.deiconify()  # 메인 창 다시 보이기
+
+    # 이전으로 돌아가는 버튼 생성
+    back_button = Button(desc_window, text="이전", command=back_to_main)
+    back_button.pack(pady=10)
+
+# 버튼 생성
+start_button = Button(main_window, text="게임 시작", command=open_stage_window)
+story_button = Button(main_window, text="스토리 설명", command=open_story_window)
+desc_button = Button(main_window, text="게임 설명", command=open_desc_window)
+quit_button = Button(main_window, text="끝내기", command=main_window.destroy)
 
 # 버튼 일자로 배치
-start_button.pack(side=LEFT, padx=10, pady=10)
-story_button.pack(side=LEFT, padx=10, pady=10)
-desc_button.pack(side=LEFT, padx=10, pady=10)
-quit_button.pack(side=LEFT, padx=10, pady=10)
+start_button.pack(side="left", padx=10, pady=10)
+story_button.pack(side="left", padx=10, pady=10)
+desc_button.pack(side="left", padx=10, pady=10)
+quit_button.pack(side="left", padx=10, pady=10)
 
-# 윈도우 실행
-window.mainloop()
+main_window.mainloop()
+
+# open_main_window()
